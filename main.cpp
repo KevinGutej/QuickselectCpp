328d5976e234cfe0a98c3eb1a2f99524af9fa7c1
KevinGutej <76398856+KevinGutej@users.noreply.github.com>
2025-04-02 22:56:24 +0200

    Added custom random number generator and replacing recursion with iteration for quickselect

--- main.cpp
+++ main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
 #include <vector>
-#include <ctime>
 
 using namespace std;
+
+class Random {
+    unsigned long long seed;
+public:
+    Random() : seed(123456789ULL) {}
+    unsigned int next() {
+        seed ^= seed << 21;
+        seed ^= seed >> 35;
+        seed ^= seed << 4;
+        return (seed & 0x7FFFFFFF);
+    }
+    int nextInt(int min, int max) {
+        return min + (next() % (max - min + 1));
+    }
+};
+
+Random rng;
 
 void swap(int &a, int &b) {
     int temp = a;
@@ -24,35 +40,34 @@ int partition(vector<int>& arr, int left, int right) {
 }
 
 int randomizedPartition(vector<int>& arr, int left, int right) {
-    int pivotIndex = left + (rand() % (right - left + 1));
+    int pivotIndex = rng.nextInt(left, right);
     swap(arr[pivotIndex], arr[right]);
     return partition(arr, left, right);
 }
 
 int quickselect(vector<int>& arr, int left, int right, int k) {
-    if (left == right) return arr[left];
-
-    int partitionIndex = randomizedPartition(arr, left, right);
-
-    if (partitionIndex == k) return arr[partitionIndex];
-    else if (partitionIndex > k) return quickselect(arr, left, partitionIndex - 1, k);
-    else return quickselect(arr, partitionIndex + 1, right, k);
+    while (left < right) {
+        int partitionIndex = randomizedPartition(arr, left, right);
+        if (partitionIndex == k) return arr[partitionIndex];
+        else if (partitionIndex > k) right = partitionIndex - 1;
+        else left = partitionIndex + 1;
+    }
+    return arr[left];
 }
 
 void printArray(const vector<int>& arr) {
     for (int i = 0; i < arr.size(); i++) {
         cout << arr[i] << " ";
     }
-    cout << endl;
+    cout << "\n";
 }
 
 int main() {
-    srand(time(0));
     vector<int> arr = {12, 3, 5, 7, 19, 26, 4, 10};
     int k = 4;
     cout << "Original array: ";
     printArray(arr);
     int result = quickselect(arr, 0, arr.size() - 1, k - 1);
-    cout << "The " << k << "-th smallest element is: " << result << endl;
+    cout << "The " << k << "-th smallest element is: " << result << "\n";
     return 0;
 }

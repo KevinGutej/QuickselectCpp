d86569aa854f433e1d31c3057a07c3cba8a6dd55
KevinGutej <76398856+KevinGutej@users.noreply.github.com>
2025-04-04 23:32:19 +0200

    Added some changes

--- main.cpp
+++ main.cpp
@@ -1,7 +1,4 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
+#include <stdio.h>
 
 class Random {
     unsigned long long seed;
@@ -11,63 +8,84 @@ public:
         seed ^= seed << 21;
         seed ^= seed >> 35;
         seed ^= seed << 4;
-        return (seed & 0x7FFFFFFF);
+        return (unsigned int)(seed & 0x7FFFFFFF);
     }
     int nextInt(int min, int max) {
-        return min + (next() % (max - min + 1));
+        return min + (int)(next() % (max - min + 1));
     }
 };
 
 Random rng;
 
-void swap(int &a, int &b) {
+void manualSwap(int &a, int &b) {
     int temp = a;
     a = b;
     b = temp;
 }
 
-int partition(vector<int>& arr, int left, int right) {
-    int pivot = arr[right];
-    int i = left;
-    for (int j = left; j < right; j++) {
-        if (arr[j] <= pivot) {
-            swap(arr[i], arr[j]);
-            i++;
+int partition(int arr[], int left, int right, int pivotIndex) {
+    int pivotValue = arr[pivotIndex];
+    manualSwap(arr[pivotIndex], arr[right]);
+    int storeIndex = left;
+
+    for (int i = left; i < right; i++) {
+        if (arr[i] < pivotValue) {
+            manualSwap(arr[i], arr[storeIndex]);
+            storeIndex++;
         }
     }
-    swap(arr[i], arr[right]);
-    return i;
+
+    manualSwap(arr[storeIndex], arr[right]);
+    return storeIndex;
 }
 
-int randomizedPartition(vector<int>& arr, int left, int right) {
-    int pivotIndex = rng.nextInt(left, right);
-    swap(arr[pivotIndex], arr[right]);
-    return partition(arr, left, right);
-}
+int quickselect(int arr[], int size, int k, int findLargest) {
+    if (k < 1 || k > size) return -1;
 
-int quickselect(vector<int>& arr, int left, int right, int k) {
-    while (left < right) {
-        int partitionIndex = randomizedPartition(arr, left, right);
-        if (partitionIndex == k) return arr[partitionIndex];
-        else if (partitionIndex > k) right = partitionIndex - 1;
-        else left = partitionIndex + 1;
+    if (findLargest) {
+        k = size - k + 1;
     }
+
+    k--;
+
+    int left = 0;
+    int right = size - 1;
+
+    while (left <= right) {
+        int pivotIndex = rng.nextInt(left, right);
+        int partitionIndex = partition(arr, left, right, pivotIndex);
+
+        if (partitionIndex == k)
+            return arr[partitionIndex];
+        else if (partitionIndex < k)
+            left = partitionIndex + 1;
+        else
+            right = partitionIndex - 1;
+    }
+
     return arr[left];
 }
 
-void printArray(const vector<int>& arr) {
-    for (int i = 0; i < arr.size(); i++) {
-        cout << arr[i] << " ";
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
     }
-    cout << "\n";
+    printf("\n");
 }
 
 int main() {
-    vector<int> arr = {12, 3, 5, 7, 19, 26, 4, 10};
+    int arr[] = {12, 3, 5, 7, 19, 26, 4, 10};
+    int size = sizeof(arr) / sizeof(arr[0]);
     int k = 4;
-    cout << "Original array: ";
-    printArray(arr);
-    int result = quickselect(arr, 0, arr.size() - 1, k - 1);
-    cout << "The " << k << "-th smallest element is: " << result << "\n";
+
+    printf("Original array: ");
+    printArray(arr, size);
+
+    int kthSmallest = quickselect(arr, size, k, 0);
+    printf("The %d-th smallest element is: %d\n", k, kthSmallest);
+
+    int kthLargest = quickselect(arr, size, k, 1);
+    printf("The %d-th largest element is: %d\n", k, kthLargest);
+
     return 0;
 }
